a5bfec7b7103a3df0c96f3c8913cf78110112adb
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-26 10:50:53 -0400

    added post and get

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,16 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import {getTasks, createTask, updateTask, deleteTask} from "./src/tasks.js"
-
+import { getTasks, createTask, updateTask, deleteTask } from "./src/tasks.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/tasks", getTasks)
-app.post("/tasks", createTask)
-app.patch("/task/:taskId", updateTask)
-app.delete("/tasks/:taskId", deleteTask)
-
+app.get("/tasks", getTasks);
+app.post("/tasks", createTask);
+app.patch("/task/:taskId", updateTask);
+app.delete("/tasks/:taskId", deleteTask);
 
 export const api = functions.https.onRequest(app);
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,24 +1,48 @@
+import dbConnect from "./dbConnect/fsConnect.js";
+
 // app.get("/tasks", getTasks)
 // app.post("/tasks", createTask)
 // app.patch("/task/:taskId", updateTask)
 // app.delete("/tasks/:taskId", deleteTask)
 
-export function getTasks(req, res) {
-    res.send("Tasks")
+export async function getTasks(req, res) {
+  const db = dbConnect();
+  const collection = await db
+    .collection("tasks")
+    .get()
+    .catch((err) => res.status(500).send(err));
+  const tasks = collection.docs.map((doc) => {
+    let task = doc.data();
+    task.id = doc.id;
+    return task;
+    //return {...doc.data(), id: doc.id}
+  });
+  res.send(tasks);
 }
 
-export function createTask(req, res) {
-    const newTask = req.body;
-    res.status(201).send("Task added")
+export async function createTask(req, res) {
+  const newTask = req.body;
+  if (!newTask || !newTask.task) {
+    //make task required
+    res.status(400).send({ success: false, message: "Invalid request" });
+  }
+  const db = dbConnect();
+  await db
+    .collection("tasks")
+    .add(newTask)
+    .catch((err) => res.status(500).send(err));
+  res.status(201);
+  getTasks(req, res); //let this function handle the response. send back the full list of tasks after this one is added
+  //.send({success: true, message:"Task added"});
 }
 
 export function updateTask(req, res) {
-    const taskUpdate = req.body;
-    const {taskId} = req.params;
-    res.status(202).send("Task updated")
+  const taskUpdate = req.body;
+  const { taskId } = req.params;
+  res.status(202).send("Task updated");
 }
 
 export function deleteTask(req, res) {
-    const {taskId} = req.params;
-    res.status(203).send("Task deleted")
+  const { taskId } = req.params;
+  res.status(203).send("Task deleted");
 }
